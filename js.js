84e74a5ca0c491e8d6b7db0ef416c0eb36692c40
UjinKP <ujin_kp@meta.ua>
2019-06-02 15:08:44 +0300

    while/doWhile/for

--- js.js
+++ js.js
@@ -30,3 +30,22 @@ switch (num) {
         break;
 }
 
+let num = 50;
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55)
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        //break;
+        continue;
+    }
+    console.log(i);
+}
